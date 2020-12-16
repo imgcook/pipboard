08b9a5f54338836909fc7422334657752215b521
sirm2z <sirm2zisv@gmail.com>
2020-12-17 00:41:25 +0800

    add CNAME file

--- scripts/pullModels.js
+++ scripts/pullModels.js
@@ -1,10 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 const download = require('download');
 const { modulesOSSUrl } = require('./config');
- 
+
 (async () => {
   Object.keys(modulesOSSUrl).forEach(item => {
     modulesOSSUrl[item].forEach(itm => {
       download(itm, `dist/static/models/${item}`);
     });
+  });
+  fs.writeFile(path.resolve(process.cwd(), './dist/CNAME'), 'pipboard.imgcook.com', (err) => {
+    if (err) throw err;
   });
 })();
