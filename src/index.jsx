1c80219e14814a659b7443dc2f826992c2b92ac4
sirm2z <sirm2zisv@gmail.com>
2020-12-20 01:06:24 +0800

    update: add @ctrl/tinycolor

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// solve the problem of parcel build dependency
+import '@ctrl/tinycolor';
 
 import Router from './router';
 import './index.less';
